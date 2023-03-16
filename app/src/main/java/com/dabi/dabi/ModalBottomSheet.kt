064efee39930aed71a510f8139151dd5a805c945
LoremIpsum1512 <huyhoangnt1512@gmail.com>
2023-03-16 14:06:10 +0700

    feat: modal bottom sheet

---
+++ app/src/main/java/com/dabi/dabi/ModalBottomSheet.kt
@@ -0,0 +1,20 @@
+package com.dabi.dabi
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dabi.dabi.data.HeightFilterValue
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class ModalBottomSheet : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.modal_bottom_sheet, container, false)
+
+    companion object {
+        const val TAG = "ModalBottomSheet"
+    }
+}
